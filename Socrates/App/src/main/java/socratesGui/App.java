078bdfad102612103cd57d706f03e2e20f68e70d
Diego QUintero <quinterocastellanos3@gmail.com>
2026-04-18 20:46:52 -0500

    arreglado

--- Socrates/App/src/main/java/socratesGui/App.java
+++ Socrates/App/src/main/java/socratesGui/App.java
@@ -29,7 +29,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/socrates/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
