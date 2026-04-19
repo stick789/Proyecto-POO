acbc6e0e4948d00ed6245140d657a0a755785237
Diego Alejandro Quintero Castellanos <diegoalejandroquintero@ucompensar.edu.co>
2026-04-19 01:34:26 +0000

    orden

--- Socrates/App/src/main/java/module-info.java
+++ Socrates/App/src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module socrates {
+module socratesGui {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
     requires java.sql;
     requires mysql.connector.j;
 
-    opens socrates to javafx.fxml;
-    exports socrates;
+    opens socratesGui to javafx.fxml;
+    exports socratesGui;
 }
