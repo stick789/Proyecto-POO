5cabacf71dcfe4de90337cc633702003e1ec8726
Diego QUintero <quinterocastellanos3@gmail.com>
2026-04-27 22:26:10 -0500

    añadido entidad pagos

---
+++ Socrates/App/src/main/java/entidades/Pagos.java
@@ -0,0 +1,51 @@
+package entidades;
+import java.math.BigDecimal;
+
+public class Pagos extends Historial_citas {
+    private Long idPago;
+    private BigDecimal monto;
+    private String metodoPago;
+    private String estadoPago;
+
+   
+
+    public Pagos(Long idPago, BigDecimal monto, String metodoPago, String estadoPago, String idTurno, Integer idUsuario) {
+        super(0, idTurno, null, null, idUsuario, null, "Pago realizado");
+        this.idPago = idPago;
+        this.monto = monto;
+        this.metodoPago = metodoPago;
+        this.estadoPago = estadoPago;
+    }
+     public Long getIdPago() {
+        return idPago;
+    }
+
+    public void setIdPago(Long idPago) {
+        this.idPago = idPago;
+    }
+
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public String getEstadoPago() {
+        return estadoPago;
+    }
+
+    public void setEstadoPago(String estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+    
+}
