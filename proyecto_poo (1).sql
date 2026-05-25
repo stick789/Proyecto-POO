22006122dcc9eaadeabe91f227c2ba1b090d3bb2
Diego QUintero <diegoalejandroquintero@ucompensar.edu.co>
2026-05-24 19:19:20 -0500

    corregido nombre de carril asignado

--- proyecto_poo (1).sql
+++ proyecto_poo (1).sql
@@ -218,7 +218,7 @@ CREATE TABLE `turno` (
   `id_usuario` int(11) NOT NULL,
   `id_instalacion` int(11) NOT NULL,
   `id_entrenador` int(11) DEFAULT NULL,
-  `numero_carril_assigned` int(11) DEFAULT NULL,
+  `numero_carril_asignado` int(11) DEFAULT NULL,
   `estado` varchar(20) DEFAULT 'RESERVADO',
   `idSede` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
